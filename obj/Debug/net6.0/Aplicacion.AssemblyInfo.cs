603eaa27c4c55f06e240f025d9d44f3eed92f6f0
Shumacker Lorenzo <shumackerlorenzo@gmail.com>
2024-05-16 10:29:33 -0400

    commit

--- obj/Debug/net6.0/Aplicacion.AssemblyInfo.cs
+++ obj/Debug/net6.0/Aplicacion.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Aplicacion")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+86912babee3609f58148807cdfacf047cb5997af")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a1826fe20964cbad6e17d232df8c641cb15bf787")]
 [assembly: System.Reflection.AssemblyProductAttribute("Aplicacion")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Aplicacion")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
